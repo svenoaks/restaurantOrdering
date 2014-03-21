8e75c25ff57d916d4089d2494e8737532a03817d
Steve <svenoaks@gmail.com>
2014-03-20 22:23:39 -0400

    cleanup

---
+++ RestaurantCommon/com/smp/restaurantcommon/Constants.java
@@ -0,0 +1,16 @@
+package com.smp.restaurantcommon;
+
+public class Constants
+{
+	public static final String DB_MENU_NAME = "entryName";
+	public static final String DB_MENU_DESCRIPTION = "description";
+	public static final String DB_MENU_BASE_PRICE = "basePrice";
+	public static final String DB_MENU_ITEMS = "menu_items";
+	
+	public static final String DB_STORE_ID = "storeID";
+	
+	public static final String JSON_MENU_NAME = "entryName";
+	public static final String JSON_MENU_DESCRIPTION = "description";
+	public static final String JSON_MENU_BASE_PRICE = "basePrice";
+	public static final String JSON_MENU_ITEMS = "menuItems";
+}
