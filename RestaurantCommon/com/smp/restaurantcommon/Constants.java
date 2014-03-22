6caf3d21713fccd612e5d499e5a74865f8d0773d
Steve <svenoaks@gmail.com>
2014-03-22 16:18:58 -0400

    before maven

--- RestaurantCommon/com/smp/restaurantcommon/Constants.java
+++ RestaurantCommon/com/smp/restaurantcommon/Constants.java
@@ -5,7 +5,7 @@ public class Constants
 	public static final String DB_MENU_NAME = "entryName";
 	public static final String DB_MENU_DESCRIPTION = "description";
 	public static final String DB_MENU_BASE_PRICE = "basePrice";
-	public static final String DB_MENU_ITEMS = "menu_items";
+	public static final String DB_MENU_ITEMS = "menuItems";
 	
 	public static final String DB_STORE_ID = "storeID";
 	
